d88ce7233f500c0715d34d98a7f01cb887c3fec4
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-06-13 17:06:51 +0200

    feat: create a new list of the first 3 article's headline and description using list comprehemsion

--- main.py
+++ main.py
@@ -16,17 +16,12 @@ data = response.json()["Time Series (Daily)"]
 data_list = [value for (key, value) in data.items()]
 yesterday_data = data_list[0]
 yesterday_closing_price = yesterday_data["4. close"]
-print(yesterday_closing_price)
 
 day_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
-print(day_before_yesterday_closing_price)
 
 difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
-print(difference)
-
 diff_percent = (difference / float(yesterday_closing_price)) * 100
-print(diff_percent)
 
 # In order to check if this is working we must set the value to a number lower than the percentage.
 # The required value for the exercise is 5
@@ -39,4 +34,8 @@ if diff_percent > 0:
     news_response = requests.get(NEWS_ENDPOINT, params=news_params)
     articles = news_response.json()["articles"]
     first_three_articles = articles[:3]
-    print(first_three_articles)
+
+    formatted_news = [
+        f"Headline: {article['title']}.\nBrief: {article['description']}" for article in first_three_articles
+    ]
+    print(formatted_news)
